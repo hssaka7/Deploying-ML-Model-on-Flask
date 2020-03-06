3227cf17d805c6e5e1586535e6b165edffe2de9e
hssaka7 <hssaka7@gmail.com>
2020-03-05 22:34:47 -0600

    added the pickle dump to save the model to the file and a test case to load the model back

--- base_model.py
+++ base_model.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
+import pickle
+
 wine_dataset = load_wine()
 
 #loading the data to pandas dataset
@@ -23,4 +25,14 @@ x_train, x_test, y_train, y_test = train_test_split(df.drop('target',axis = 1), 
 md = DecisionTreeClassifier().fit(x_train, y_train)
 print("accuracy is {0}".format(md.score(x_test, y_test)))
 
+
+# need to save the model using pickle to the file
+file_destination = "model/classifier_dump.sav"
+pickle.dump(md,open(file_destination, 'wb'))
+
+# testing the dumped model by loading the model
+model_loaded = pickle.load(open(file_destination, 'rb'))
+accuracy = model_loaded.score(x_test, y_test)
+print(accuracy)
+# the accuracy matches
 
